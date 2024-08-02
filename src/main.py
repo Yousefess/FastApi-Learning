61cae9a9fee63bd73dcbbf334292f882a8594987
Yousefess <yousefe660@gmail.com>
2024-08-02 17:35:36 +0330

    Update Content

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -6,3 +7,15 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello World!"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts"}
+
+
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+    # extract all the field from the body and store inside variable name "payload"
+    print(payload)
+    return {"new_post": f"title: {payload['title']}, content: {payload['content']}"}
